c24f408a18af37d14250d21d75c179d19383f009
daslu <daniel.slutsky@gmail.com>
2021-01-16 19:02:45 +0200

    some progress with the passengers stationarity tutorial

--- projects/timeseries-passenger-demo/passengers/src/passengers/core.clj
+++ projects/timeseries-passenger-demo/passengers/src/passengers/core.clj
@@ -1,16 +1,23 @@
 (ns passengers.core
   (:require [notespace.api :as notespace]))
 
+["Note: We purposely overridden the `clojure.core` arithmetic operations with the `dtype-next` ones, to support nice array-arithmetic."]
+
 (require '[notespace.kinds :as kind]
          '[tech.v3.dataset :as dataset]
          '[tablecloth.api :as tablecloth]
          '[tech.viz.vega :as viz]
          '[tech.v3.datatype :as dtype]
-         '[tech.v3.datatype.functional :as dtype-fun]
+         '[tech.v3.datatype.functional :as dtype-fun
+           :refer [+ - * /]]
          '[tech.v3.datatype.rolling :as dtype-roll]
          '[tech.v3.datatype.datetime :as dtype-dt]
          '[aerial.hanami.common :as hanami-common]
          '[aerial.hanami.templates :as hanami-templates])
+
+(+ 4 5)
+
+(+ [2 4] [3 5])
 
 (def path "../data/AirPassengers.csv")
 
@@ -67,13 +74,22 @@ passengers-with-log
 
 (def passengers-with-rolling
   (-> passengers-with-log
-      (tablecloth/add-or-replace-column
-       :rolling-mean
-       #(-> %
-            (:log-passengers)
-            (dtype-roll/fixed-rolling-window
-             12
-             dtype-fun/mean {:relative-window-position :left})))))
+      (tablecloth/add-or-replace-columns
+       ;; TODO: How can we not duplicate code here?
+       {:rolling-mean
+        #(-> %
+             (:log-passengers)
+             (dtype-roll/fixed-rolling-window
+              12
+              dtype-fun/mean
+              {:relative-window-position :left}))
+        :rolling-standard-deviation
+        #(-> %
+             (:log-passengers)
+             (dtype-roll/fixed-rolling-window
+              12
+              dtype-fun/standard-deviation
+              {:relative-window-position :left}))})))
 
 ;; This seems not to use the window on the left. 
 (dtype-roll/fixed-rolling-window (range 20) 10 dtype-fun/sum {:relative-window-position :left})
@@ -90,3 +106,66 @@ passengers-with-rolling
  :Y :rolling-mean
  :YSCALE {:zero false})
 
+^kind/hiccup
+[:div
+ [:p/vega
+  (-> (hanami-common/xform
+       hanami-templates/line-chart
+       :DATA (prep-data-for-plotting passengers)
+       :X :Month
+       :XTYPE :temporal
+       :Y :#Passengers))]
+ [:p/vega
+  (hanami-common/xform
+   hanami-templates/line-chart
+   :DATA (prep-data-for-plotting passengers-with-log)
+   :X :Month
+   :XTYPE :temporal
+   :Y :log-passengers
+   :YSCALE {:zero false})]
+ [:p/vega
+  ^kind/vega
+  (hanami-common/xform
+   hanami-templates/line-chart
+   :DATA (prep-data-for-plotting passengers-with-rolling)
+   :X :Month
+   :XTYPE :temporal
+   :Y :rolling-mean
+   :YSCALE {:zero false})]]
+
+
+^kind/vega
+(hanami-common/xform
+ hanami-templates/layer-chart
+ :DATA (prep-data-for-plotting passengers-with-rolling)
+ :LAYER [(hanami-common/xform
+          hanami-templates/line-chart
+          :X :Month
+          :XTYPE :temporal
+          :Y :log-passengers
+          :YSCALE {:zero false})
+         (hanami-common/xform
+          hanami-templates/line-chart
+          :X :Month
+          :XTYPE :temporal
+          :Y :rolling-mean
+          :YSCALE {:zero false}
+          :MCOLOR "red")])
+
+(def passengers-with-difference
+  (-> passengers-with-rolling
+      (tablecloth/add-or-replace-column
+       :difference #(- (:log-passengers %)
+                       (:rolling-mean %)))))
+
+
+^kind/vega
+(hanami-common/xform
+ hanami-templates/layer-chart
+ :DATA (prep-data-for-plotting passengers-with-difference)
+ :LAYER [(hanami-common/xform
+          hanami-templates/line-chart
+          :X :Month
+          :XTYPE :temporal
+          :Y :difference
+          :YSCALE {:zero false})])
