56db087d10624bb515538449f91b270c80757994
Ethan Miller <ethanzanemiller@gmail.com>
2021-04-17 08:08:47 -0700

    Save work on tmd column index extensibility demo

--- projects/index-experiments/project.clj
+++ projects/index-experiments/project.clj
@@ -3,7 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :plugins [[reifyhealth/lein-git-down "0.4.0"]]
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [scicloj/tablecloth "5.00-beta-27"]
-                 [scicloj/notespace "3-alpha3-SNAPSHOT"]]
-  :repl-options {:init-ns index-experiments.core})
+                 [scicloj/tablecloth "5.11"]
+                 [factual/geo "4566f37e2681113373350632087d2dbf8f145b96"]
+                 [techascent/tech.ml.dataset "5.13-SNAPSHOT"]
+                 [scicloj/notespace "3-beta5"]
+                 [scicloj/tablecloth.time "1414cee38d634e62d34a9f0aebeca93f64dbdea9"]]
+  :repositories [["public-github" {:url "git://github.com"}]]
+  )
---
+++ projects/index-experiments/src/index_experiments/tmd_column_extension_demo.clj
@@ -0,0 +1,115 @@
+(ns index-experiments.tmd-column-extension-demo
+  (:require [tech.v3.dataset :as ds]
+            [tech.v3.dataset.column :as ds-col]
+            [tech.v3.datatype :as dtype]
+            [tech.v3.datatype.casting :as casting]
+            [tablecloth.api :as tablecloth]
+            [geo
+             [geohash :as geohash]
+             [jts :as jts]
+             [spatial :as spatial]
+             [io :as geoio]
+             [crs :as crs]]
+            [notespace.api]))
+
+(import (org.locationtech.jts.index.strtree STRtree)
+        (org.locationtech.jts.geom Geometry Point Polygon Coordinate)
+        (org.locationtech.jts.geom.prep PreparedGeometry
+                                        PreparedLineString
+                                        PreparedPolygon
+                                        PreparedGeometryFactory)
+        (java.util TreeMap))
+
+;; https://epsg.io/4326
+(def wgs84-crs (crs/create-crs 4326))
+
+
+;; https://epsg.io/6539
+;; Center coordinates
+;; 1252460.55256480 263192.27520000
+;; Projected bounds:
+;; 911908.37699881 110617.87078182
+;; 1588713.88595833 420506.52709693
+(def nad83-2011-crs (crs/create-crs 6539))
+
+(def crs-transform
+  (crs/create-transform wgs84-crs nad83-2011-crs))
+
+(defn wgs84->nad83-2011
+  "Transforming latitude-longitude coordinates
+  to local Eucledian coordinates around NYC."
+  [geometry]
+  (jts/transform-geom geometry crs-transform))
+(def get-neighbourhoods-data
+  (memoize
+   (fn []
+     (-> "data/nyc-neighbourhoods.json"
+         slurp
+         geoio/read-geojson))))
+
+(take 1 (get-neighbourhoods-data))
+
+(defn polygon->latlngs
+  "Convert a polygon to a sequence of longitude-latitude pairs, assuming it is in the appropriate coordinate reference system."
+  [polygon]
+  (->> polygon
+       jts/coordinates
+       (map (juxt spatial/latitude
+                  spatial/longitude))))
+
+(def get-neighbourhoods
+  (memoize
+   (fn []
+     (-> (get-neighbourhoods-data)
+         (->> (map (fn [{:keys [geometry properties]}]
+                     (assoc properties :geometry geometry))))
+         tablecloth/dataset
+         (tablecloth/rename-columns {(keyword "@id") :id})
+         (tablecloth/add-or-replace-column
+          :geometry #(map wgs84->nad83-2011
+                          (:geometry %)))
+         (vary-meta assoc :print-column-max-width 100)
+         ))))
+
+^kind/dataset
+(get-neighbourhoods)
+
+
+(-> (get-neighbourhoods)
+    :geometry
+    first
+    dtype/elemwise-datatype
+    casting/datatype->object-class
+    )
+
+;; Make sure tech.datatype knows about the datatype you are working with.
+;; Which you test by calling tech.v3.datatype.casting/elemwise-datatype on the data.
+(casting/add-object-datatype! :geometry Geometry true)
+
+;; Extend the multimethod 
+(defmethod tech.v3.dataset.impl.column-index-structure/make-index-structure Geometry
+  [data _]
+  []
+  )
+
+  ;; (let [tree ^STRtree (STRtree.)]
+  ;;   (doseq [[index-position geometry] (map-indexed vector data)]
+  ;;     (.insert tree
+  ;;              (.getEnvelopeInternal geometry)
+  ;;              {:index-position index-position
+  ;;               :prepared-geometry (PreparedGeometryFactory/prepare geometry)}))
+  ;;   tree)
+
+(->
+    (get-neighbourhoods)
+    :geometry
+    ds-col/index-structure
+    ;; type
+    ;; (with-index-structure [this datatype-keyword klass make-index-structure-fn]
+    ;; (.with-index-structure :geometry Geometry (fn [data meta] []))
+    ;; ds-col/index-structure
+    ;; ds-col/index-structure
+    )
+
+
+
