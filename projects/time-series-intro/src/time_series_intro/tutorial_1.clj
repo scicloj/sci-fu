7bb983354cb310748aae4adefcc9682543a2f107
Ethan Miller <ethanzanemiller@gmail.com>
2021-07-08 15:26:48 -0700

    Add to tutorial 1

--- projects/time-series-intro/src/time_series_intro/tutorial_1.clj
+++ projects/time-series-intro/src/time_series_intro/tutorial_1.clj
@@ -5,7 +5,9 @@
 (import '[java.time.format DateTimeFormatter])
 
 (require '[tablecloth.api :as tbl]
+         '[tablecloth.time.api :as time]
          '[clojure.string :refer [lower-case]])
+
 
 (def data (tbl/dataset "data/aapl.csv"
                        {:key-fn keyword
@@ -13,8 +15,24 @@
                                            (DateTimeFormatter/ofPattern "d-MMM-yy")]}}))
 
 
+
 ^kind/dataset
 (tbl/head data)
 
 ^kind/dataset
 (tbl/info data)
+
+^kind/md-nocode
+["One way to get all entries for a given year is to use slice."]
+
+;; Could slice be made more flexible now so the user could also do justify
+;; (time/slice "2017")?
+^kind/dataset
+(-> data
+    (time/slice "2017-01-01" "2017-12-31"))
+
+;; Again here the story around years is confusing. Probably just need a
+;; ->year, and this function will return a number?
+^kind/dataset
+(-> data
+    (tbl/select-rows (comp #(= % 2017) #(.getYear %) :Date)))
